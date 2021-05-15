f6f26575f449e2b6721e9bfd5d1a2fe50fdce11d
MicrolemurDev <microlemurdev@microlemur.net>
2021-05-14 22:31:32 -0500

    Start App Searcher

---
+++ shared/appsearch.js
@@ -0,0 +1,39 @@
+const exe=require('executor.js'); // So we can launch bash scripts and launch games :P
+const path = require('path');
+const fs=require('fs');
+let commands=[]; // This is the whole command list
+let command_length=0;
+let installed=[]; // List of installed apps that work with g7os
+let installed_length=0;
+function fromDir(startPath,filter){
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) {
+            // List
+           commands[command_length]=filename;
+           command_length+=1;
+        };
+    };
+}; // Adopted from here (https://stackoverflow.com/questions/25460574/find-files-by-extension-html-under-a-folder-in-nodejs)
+function genConfig(){
+let i001 = commands.indexOf("dolphin-emu"); // Dolphin Emulator!
+  if(i001!=-1){
+  installed[installed_length]='dolphin-emu';
+  installed_length+=1;
+  }
+}
+fromDir('/usr/bin/',''); // List all files (This takes a bit)
+// Time for the main event! The config generator! (Generates a list of all apps/emulators that integrate with g7os!) (This list is found in /g7/apps.cfg)
