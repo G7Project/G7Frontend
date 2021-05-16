6d88ec4879a186696f1e1ddbfde948f266b8ca84
MicrolemurDev <microlemurdev@microlemur.net>
2021-05-16 18:21:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ echo 'G7Frontend 1.0 Installer'
 echo '(C) 2021 Microlemur, Licensed under the MIT License'
 echo '====================================================='
 echo 'Ensuring Base Packages are installed....'
-sudo pacman -S base-devel nodejs npm electron --noconfirm # We need this
-echo 'Creating Syslink in /usr/bin...'
-sudo mv shared/g7frontend.sh /usr/bin/
+sudo pacman -S base-devel nodejs npm --noconfirm # We need this
+npm install "https://github.com/castlabs/electron-releases#11.4.6-wvvmp" --save-dev
+echo 'Installing NodeJS App...'
+npm install
 echo 'Done!'
