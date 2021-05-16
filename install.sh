af48e9e87d2a58fe053f2576895f2fcd0d5d37ea
MicrolemurDev <microlemurdev@microlemur.net>
2021-05-16 16:54:46 -0500

    This will work *for now*

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # This just installs G7Frontend to your computer
 # P.S. you need sudo
-sudo pacman -S base-devel nodejs npm # We need this
+echo 'G7Frontend 1.0 Installer'
+echo '(C) 2021 Microlemur, Licensed under the MIT License'
+echo '====================================================='
+echo 'Ensuring Base Packages are installed....'
+sudo pacman -S base-devel nodejs npm electron --noconfirm # We need this
+echo 'Creating Syslink in /usr/bin...'
+sudo mv shared/g7frontend.sh /usr/bin/
+echo 'Done!'
