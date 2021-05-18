b85ecb47a1a33d675550dc49f95fa55691bc38e8
G7Project <84022239+G7Project@users.noreply.github.com>
2021-05-18 08:45:44 -0500

    We need to add module.exports

--- .hidden/steamexec.js
+++ .hidden/steamexec.js
@@ -1,6 +1,7 @@
 // Executing Steam Games is Fun!
 // P.S. this is a library so we can include it to launch steam games :D
 const ex=require('/g7/shared/executor.js');
+const { readdirSync } = require('fs') //File System needed
 function exec_STEAM(game,arch){
   console.log('Checking Arch @ exec_STEAM...');
 if(arch!='x86' && arch!='x86_64'){
@@ -8,4 +9,5 @@ throw 'Error: Invalid Architecture for game defined.';
  }
   console.log('Booting Game...');
   execute('exec ~/.steam/steam/steamapps/common/'+game+'.'+arch); //This line does all the real magic
-}
+} // This runs steam games
+module.exports = {exec_STEAM};
